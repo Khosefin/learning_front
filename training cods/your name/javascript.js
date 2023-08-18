87cca6d09c848142ea98feb700391c2f09094cfb
Arian shoughi <arianshoughi@gmail.com>
2023-08-18 16:21:26 +0330

    Sort files & Deleted duplicate files

--- training cods/your name/javascript.js
+++
@@ -1,8 +0,0 @@
-let username;
-
-document.getElementById("myButton").onclick = function () {
-
-  username = document.getElementById("myText").value
-  document.getElementById("myLable").innerHTML = username;
-
-};
