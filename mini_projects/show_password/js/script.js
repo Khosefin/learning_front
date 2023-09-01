a0360ba1417f2c7b6c0be75743a7c4da4102b092
Arian shoughi <arianshoughi@gmail.com>
2023-09-01 20:21:17 +0330

    added show password to input

---
+++ mini_projects/show_password/js/script.js
@@ -0,0 +1,21 @@
+﻿var $ = document;
+function _id(id_name) {
+  return $.getElementById(id_name);
+}
+
+function _class(class_name) {
+  return $.getElementsByClassName(class_name);
+}
+
+var togglePassword = _class("toggle-password");
+var passwordField = _id("password-field");
+
+togglePassword[0].addEventListener("click", function () {
+  if (passwordField.type == "text") {
+    passwordField.type = "password";
+    togglePassword[0].classList.remove("active");
+  } else {
+    passwordField.type = "text";
+    togglePassword[0].classList.add("active");
+  }
+});
