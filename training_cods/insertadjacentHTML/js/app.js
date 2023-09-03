24b630d519197c6c2168753ff736820c76b1c45c
Arian shoughi <arianshoughi@gmail.com>
2023-09-04 02:40:52 +0330

    working with insertAdjacentHTML method

---
+++ training_cods/insertadjacentHTML/js/app.js
@@ -0,0 +1,6 @@
+const boxElem = document.querySelector('.box')
+
+// boxElem.insertAdjacentHTML('afterbegin', '<li>Courses</li>')
+// boxElem.insertAdjacentHTML('beforeend', '<li>Courses</li>')
+// boxElem.insertAdjacentHTML('beforebegin', '<li>Courses</li>')
+boxElem.insertAdjacentHTML('afterend', '<li>Courses</li>')
