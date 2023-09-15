dc8a351b86e29ae37eac3de076c67e707e5497e7
Arian shoughi <arianshoughi@gmail.com>
2023-09-16 03:02:07 +0330

    custom scroll bar miniproject

---
+++ mini_projects/scroll_bar/js/app.js
@@ -0,0 +1,15 @@
+let customScroll = document.querySelector("#scroll");
+
+window.addEventListener("scroll", function () {
+  let scrollTop = window.scrollY;
+
+  let documentHeight = document.body.clientHeight;
+
+  let windowHeight = window.innerHeight;
+
+  let scrollPercent = scrollTop / (documentHeight - windowHeight);
+
+  let scrollPercentRounded = Math.round(scrollPercent * 100);
+
+  customScroll.style.width = scrollPercentRounded + "%";
+});
